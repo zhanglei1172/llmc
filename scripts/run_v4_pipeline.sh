a8fa4db9a5fccbc31db3dede61af24914a6d5409
zhangl98@xiaopeng.com <LeiZhang>
2025-08-04 06:47:41 +0000

    add vit quarot

--- scripts/run_v4_pipeline.sh
+++ scripts/run_v4_pipeline.sh
@@ -1,9 +1,13 @@
 export python_path='/workspace/zhangl98@xiaopeng.com/miniconda3/envs/q/bin/python'
 export CUDA_VISIBLE_DEVICES=0,1
 export WANDB_MODE=offline
-export HF_ENDPOINT="https://hf-mirror.com"
+export HF_ENDPOINT="https://alpha.hf-mirror.com"
+export HF_DATASETS_CACHE="/workspace/zhangl98@xiaopeng.com/hf_cache/"
 
 set -xe
+
+# huggingface-cli login first
+PYTHONPATH='.' llmc=./llmc ${python_path} -m torch.distributed.run --nnode 1 --nproc_per_node 1 --rdzv_id 1175 --rdzv_backend c10d --rdzv_endpoint localhost:12358 ./llmc/__main__.py --config configs/quantization/combination/quarot_comb_gptq/w8a8/v4_test_step_0_vision_quarot.yml --task_id 1175  > log_vit_quarot.log 2>&1
 
 PYTHONPATH='.' llmc=./llmc ${python_path} -m torch.distributed.run --nnode 1 --nproc_per_node 2 --rdzv_id 1175 --rdzv_backend c10d --rdzv_endpoint localhost:12358 ./llmc/__main__.py --config configs/quantization/combination/quarot_comb_gptq/w8a8/v4_test_step_1_spinquant_fsdp.yml --task_id 1175 > log_spinqunat.log 2>&1
 
